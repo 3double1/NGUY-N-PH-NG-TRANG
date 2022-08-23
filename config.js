5a30d82f32a30dc03004047e1347231fdbccf82a
Nguyễn Trọng Nam <89219120+3double1@users.noreply.github.com>
2022-08-23 17:54:11 +0700

    Add files via upload
    HAPPY

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bạn iu';
+const giftUrl = 'https://www.facebook.com/nguyen.minhhuong.37853';
+const eventName = 'HAPPY BIRTHDAY';
+const titleCard = 'Tặng Nguyễn Phương Trang';
+const contentCard = 'Chúc mừng sinh nhật bạn, chúc bạn tuổi mới sẽ có thật nhiều niềm vui, hạnh phúc và may mắn, chúc bạn luôn luôn có một sức khỏe thật dồi dào để không bao giờ phải cảm thấy mệt zlzl, và chúc mừng bạn đã già thêm một tuổi, Lò vé, và sẽ sớm có được tui';
+
+const giftImage = 'NY.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
